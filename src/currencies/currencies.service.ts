70db709ed44412570822cd3ef54d1eaee3a009c7
NeofitoEOF <lorenconi16@gmail.com>
2023-08-05 17:01:15 -0300

    feat: add method in class

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CurrenciesService {}
+export class CurrenciesService {
+  async getCurrency(currency: string): Promise<any> {}
+}
