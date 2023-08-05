e124ddc579ae718f36995113a74521b3d3163680
NeofitoEOF <lorenconi16@gmail.com>
2023-08-05 15:20:50 -0300

    test: ensure convertAmount called getCurrency with correct params

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -42,5 +42,10 @@ describe('ExchangeService', () => {
       await service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 });
       await expect(currenciesService.getCurrency).toBeCalledTimes(2);
     });
+    it('should be called getCurrency with correct params', async () => {
+      await service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 });
+      await expect(currenciesService.getCurrency).toBeCalledWith('USD');
+      await expect(currenciesService.getCurrency).toBeCalledWith('BRL');
+    });
   });
 });
