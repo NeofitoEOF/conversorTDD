c55b1e207e32e132231723b4960a340e49e61d3e
NeofitoEOF <lorenconi16@gmail.com>
2023-08-05 15:56:08 -0300

    test: ensure return conversion correct

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -8,7 +8,7 @@ describe('ExchangeService', () => {
 
   beforeEach(async () => {
     const currenciesServiceMock = {
-      getCurrency: jest.fn(),
+      getCurrency: jest.fn().mockReturnValue({ value: 1 }),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -54,6 +54,27 @@ describe('ExchangeService', () => {
       await expect(
         service.convertAmount({ from: 'INVALID', to: 'BRL', amount: 1 }),
       ).rejects.toThrow();
+    });
+    it('should be return conversion value', async () => {
+      (currenciesService.getCurrency as jest.Mock).mockResolvedValueOnce({
+        value: 1,
+      });
+      (currenciesService.getCurrency as jest.Mock).mockResolvedValueOnce({
+        value: 0.205,
+      });
+      expect(
+        await service.convertAmount({ from: 'USD', to: 'BRL', amount: 1 }),
+      ).toEqual({ amount: 4.878048780487805 });
+
+      (currenciesService.getCurrency as jest.Mock).mockResolvedValueOnce({
+        value: 0.205,
+      });
+      (currenciesService.getCurrency as jest.Mock).mockResolvedValueOnce({
+        value: 1,
+      });
+      expect(
+        await service.convertAmount({ from: 'BRL', to: 'USD', amount: 1 }),
+      ).toEqual({ amount: 0.205 });
     });
   });
 });
