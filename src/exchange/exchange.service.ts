50971de06bba10a6b1add65789692f48c7c8ee4e
NeofitoEOF <lorenconi16@gmail.com>
2023-08-05 15:17:49 -0300

    feat: applying currency converter

--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,10 +1,19 @@
 import { BadGatewayException, Injectable } from '@nestjs/common';
 
+export class CurrenciesService {
+  async getCurrency(currency: string): Promise<any> {}
+}
+
 @Injectable()
 export class ExchangeService {
+  constructor(private currencyService: CurrenciesService) {}
+
   async convertAmount({ from, to, amount }): Promise<any> {
     if (!from || !to || !amount) {
       throw new BadGatewayException();
     }
+
+    const currencyFrom = this.currencyService.getCurrency(from);
+    const currencyTo = this.currencyService.getCurrency(to);
   }
 }
